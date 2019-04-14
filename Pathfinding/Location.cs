d128ad15a0ef02e498f607732bca869a684a91bb
chernokm <mac_chernok@yahoo.com>
2019-04-13 20:46:33 -0400

    Add project files.

---
+++ Pathfinding/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathfinding
+{
+    class Location
+    {
+        public int X;
+        public int Y;
+        public int F;
+        public int G;
+        public int H;
+        public Location Parent;
+    }
+}
